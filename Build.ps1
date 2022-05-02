0bff7a59fcbf86f1ca683762634076dd0f3943a0
Nicholas Dechert <dechert.nicholas@gmail.com>
2022-05-02 08:09:21 -0700

    Differentiate local builds

--- Build.ps1
+++ Build.ps1
@@ -5,7 +5,10 @@ param (
     $LTSC = "2022"
 )
 
-$Tag = "dechertnicholas/space-engineers-ds-windows:ltsc$LTSC-local"
+$Tag = "dechertnicholas/space-engineers-ds-windows:ltsc$LTSC"
+if (-not $env:GITHUB_ACTIONS) {
+    $Tag += "-local" # Differentiate local builds
+}
 $LTSC = "mcr.microsoft.com/windows/servercore:ltsc$LTSC"
 
 docker build --isolation hyperv --build-arg "LTSC=$LTSC" . -t $Tag
